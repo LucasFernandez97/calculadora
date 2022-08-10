c461a915e65805ffd990efb6a382e4ee547dff14
eloyaguado <eloy.aguado.arias@gmail.com>
2022-08-10 17:46:16 +0200

    Metodos suma y resta de dos parametros

--- src/main/java/com/prueba/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/prueba/calculadora/controller/CalculadoraController.java
@@ -1,11 +1,61 @@
 package com.prueba.calculadora.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.prueba.calculadora.service.CalculadoraService;
 
 @RestController
 @RequestMapping("/api")
 public class CalculadoraController {
 
-
+	@Autowired
+	private CalculadoraService calculadoraService;
+	
+	@GetMapping("/suma")
+	public double sumaSimple(
+			@RequestParam(value = "num1", defaultValue = "0") double num1,
+			@RequestParam(value = "num2", defaultValue = "0") double num2
+			) {
+		return calculadoraService.suma(num1, num2);
+	}
+	
+	@GetMapping("/resta")
+	public double restaSimple(
+			@RequestParam(value = "num1", defaultValue = "0") double num1,
+			@RequestParam(value = "num2", defaultValue = "0") double num2
+			) {
+		return calculadoraService.resta(num1, num2);
+	}
+	
+	/**
+	 * TODO implementacion de multiplicacion
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	@GetMapping("/multiplicacion")
+	public double multiplicacionSimple(
+			@RequestParam(value = "num1", defaultValue = "0") double num1,
+			@RequestParam(value = "num1", defaultValue = "0") double num2
+			) {
+		return 0;
+	}
+	
+	/**
+	 * TODO implementacion de division
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	@GetMapping("/division")
+	public double divisionSimple(
+			@RequestParam(value = "num1", defaultValue = "0") double num1,
+			@RequestParam(value = "num1", defaultValue = "0") double num2
+			) {
+		return 0;
+	}
 }
--- src/main/java/com/prueba/calculadora/service/CalculadoraService.java
+++ src/main/java/com/prueba/calculadora/service/CalculadoraService.java
@@ -1,8 +1,55 @@
 package com.prueba.calculadora.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculadoraService {
 
+	BigDecimal resultado;
+	
+	public double suma (double num1, double num2) {
+		
+		try {
+			
+			resultado = BigDecimal.valueOf(num1);
+			resultado = resultado.add(BigDecimal.valueOf(num2));
+			
+		} catch (NumberFormatException nfe) {
+			
+			nfe.printStackTrace();
+			return 0;
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			return 0;
+			
+		}
+		
+		return resultado.doubleValue();
+		
+	}
+	
+	public double resta (double num1, double num2) {
+		try {
+			
+			resultado = BigDecimal.valueOf(num1);
+			resultado = resultado.subtract(BigDecimal.valueOf(num2));
+			
+		} catch (NumberFormatException nfe) {
+			
+			nfe.printStackTrace();
+			return 0;
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			return 0;
+			
+		}
+		
+		return resultado.doubleValue();
+	}
 }
