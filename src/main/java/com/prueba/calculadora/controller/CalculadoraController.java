93c3c4b6f4441d675aab1640d4f34bd96269a38d
eloyaguado <eloy.aguado.arias@gmail.com>
2022-08-10 17:13:10 +0200

    Creacion controller y servicio

---
+++ src/main/java/com/prueba/calculadora/controller/CalculadoraController.java
@@ -0,0 +1,11 @@
+package com.prueba.calculadora.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CalculadoraController {
+
+
+}
