e425e372040962e31182373862ca74789d28540d
eloyaguado <eloy.aguado.arias@gmail.com>
2022-08-10 20:05:13 +0200

    Implementacion de la libreria tracer para trazas de los resultados

--- src/main/java/com/prueba/calculadora/service/CalculadoraService.java
+++ src/main/java/com/prueba/calculadora/service/CalculadoraService.java
@@ -4,22 +4,38 @@ import java.math.BigDecimal;
 
 import org.springframework.stereotype.Service;
 
+import io.corp.calculator.TracerImpl;
+
 @Service
 public class CalculadoraService {
+	
+	//TODO
+	//@Autowired
+	TracerImpl tracerImpl;
 
 	BigDecimal resultado;
 	
 	public double suma (double num1, double num2) {
-					
+			
 		resultado = BigDecimal.valueOf(num1);
-		return resultado.add(BigDecimal.valueOf(num2)).doubleValue();
+		resultado = resultado.add(BigDecimal.valueOf(num2));
+		
+		tracerImpl = new TracerImpl();
+		tracerImpl.trace(resultado);
+		
+		return resultado.doubleValue();
 					
 	}
 	
 	public double resta (double num1, double num2) {
 			
 		resultado = BigDecimal.valueOf(num1);
-		return resultado.subtract(BigDecimal.valueOf(num2)).doubleValue();
+		resultado = resultado.subtract(BigDecimal.valueOf(num2));
+	
+		tracerImpl = new TracerImpl();
+		tracerImpl.trace(resultado);
+		
+		return resultado.doubleValue();
 			
 	}
 }
