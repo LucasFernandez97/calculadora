b2fedf5d5acf6883b6bd5fea025c9c4fc652b427
eloyaguado <eloy.aguado.arias@gmail.com>
2022-09-06 19:26:38 +0200

    Mas casos de prueba y test de endpoints

--- src/test/java/com/prueba/calculadora/CalculadoraServiceTest.java
+++ src/test/java/com/prueba/calculadora/CalculadoraServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.TypeMismatchException;
 
 import com.prueba.calculadora.service.CalculadoraService;
 
@@ -22,12 +23,77 @@ public class CalculadoraServiceTest {
     public void testSumaExito() {
     	
     	Assertions.assertEquals(calculadoraService.suma(1.0, 1.0), 2);
+    	Assertions.assertEquals(calculadoraService.suma(-1.0, 1.0), 0);
+    	Assertions.assertEquals(calculadoraService.suma(1.0, -1.0), 0);
+    	Assertions.assertEquals(calculadoraService.suma(-1.0, -1.0), -2.0);
+
     }
     
     @Test
     public void testSumaFallo() {
     	
     	Assertions.assertNotEquals(calculadoraService.suma(1.0, 1.0), 3);
+    	Assertions.assertNotEquals(calculadoraService.suma(-1.0, 1.0), 3);
+    	Assertions.assertNotEquals(calculadoraService.suma(1.0, -1.0), 5);
+    	Assertions.assertNotEquals(calculadoraService.suma(-1.0, -1.0), -3.0);
+    	
+    }
+    
+    @Test
+    public void testSumaErrorParametros() {
+    	
+    	Assertions.assertThrows(NumberFormatException.class, () -> {calculadoraService.suma(Double.parseDouble("a"), 1.0);});
+    	Assertions.assertThrows(NumberFormatException.class, () -> {calculadoraService.suma(1.0, Double.parseDouble("a"));});
+    	Assertions.assertThrows(NumberFormatException.class, () -> {calculadoraService.suma(Double.parseDouble("a"), Double.parseDouble("a"));});
+
+    }
+    
+    @SuppressWarnings("null")
+	@Test
+    public void testSumaNull() {
+    	
+    	Assertions.assertThrows(NullPointerException.class, () -> {calculadoraService.suma((Double) null, 1.0);});
+    	Assertions.assertThrows(NullPointerException.class, () -> {calculadoraService.suma(1.0, (Double) null);});
+    	Assertions.assertThrows(NullPointerException.class, () -> {calculadoraService.suma((Double) null, (Double) null);});
+
+    }
+    
+    @Test
+    public void testRestaExito() {
+    	
+    	Assertions.assertEquals(calculadoraService.resta(1.0, 1.0), 0);
+    	Assertions.assertEquals(calculadoraService.resta(-1.0, 1.0), -2.0);
+    	Assertions.assertEquals(calculadoraService.resta(1.0, -1.0), 2.0);
+    	Assertions.assertEquals(calculadoraService.resta(-1.0, -1.0), 0);
+
+    }
+    
+    @Test
+    public void testRestaFallo() {
+    	
+    	Assertions.assertNotEquals(calculadoraService.resta(1.0, 1.0), 3);
+    	Assertions.assertNotEquals(calculadoraService.resta(-1.0, 1.0), 3);
+    	Assertions.assertNotEquals(calculadoraService.resta(1.0, -1.0), 5);
+    	Assertions.assertNotEquals(calculadoraService.resta(-1.0, -1.0), -3.0);
+    	
+    }
+    
+    @Test
+    public void testRestaErrorParametros() {
+    	
+    	Assertions.assertThrows(NumberFormatException.class, () -> {calculadoraService.resta(Double.parseDouble("a"), 1.0);});
+    	Assertions.assertThrows(NumberFormatException.class, () -> {calculadoraService.resta(1.0, Double.parseDouble("a"));});
+    	Assertions.assertThrows(NumberFormatException.class, () -> {calculadoraService.resta(Double.parseDouble("a"), Double.parseDouble("a"));});
+    
     }
 
+    @SuppressWarnings("null")
+	@Test
+    public void testRestaNull() {
+    	
+    	Assertions.assertThrows(NullPointerException.class, () -> {calculadoraService.resta((Double) null, 1.0);});
+    	Assertions.assertThrows(NullPointerException.class, () -> {calculadoraService.resta(1.0, (Double) null);});
+    	Assertions.assertThrows(NullPointerException.class, () -> {calculadoraService.resta((Double) null, (Double) null);});
+
+    }
 }
---
+++ src/test/java/com/prueba/calculadora/HttpRequestTest.java
@@ -0,0 +1,68 @@
+package com.prueba.calculadora;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void noHandlerFoundExceptionTest() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/asd",
+				String.class)).contains("La pagina no existe");
+	}
+	
+	@Test
+	public void typeMismatchTest() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/suma?num1=asd&num2=2",
+				String.class)).contains("Type mismatch");
+	}
+	
+	@Test
+	public void errorFormatoTest() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/suma?num1=1&num2=23423423454"
+				+ "980895675675678678978908234234234546457568768678978980895675675678678978908234234234546457568768"
+				+ "678978980895675675678678978908234234234546457568768678978980895675675678678978908234234234546457"
+				+ "568768678978980895675675678678978908234234234546457568768678978980895675675678678978908234234234"
+				+ "546457568768678978980895675675678678978908234234234546457568768678978980895675675678678978908234"
+				+ "342345464575687686789789808956756756786789789082342342345464575687686789789808956756756786789789"
+				+ "082342342345464575687686789789808956756756786789789082342342345464575687686789789808956756756786"
+				+ "789789082342342345464575687686789789808956756756786789789082342342345464575687686789789808956756"
+				+ "756786789789082342342345464575687686789789808956756756786789789082342342345464575687686789789808",
+				String.class)).contains("Error de formato");
+	}
+	
+	@Test
+	public void faltaParametroSumaTest() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/suma?num2=123",
+				String.class)).contains("123");
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/suma?num1=123",
+				String.class)).contains("123");
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/suma",
+				String.class)).contains("0");
+	}
+	
+	@Test
+	public void faltaParametroRestaTest() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/resta?num2=123",
+				String.class)).contains("-123");
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/resta?num1=123",
+				String.class)).contains("123");
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/resta",
+				String.class)).contains("0");
+	}
+}
