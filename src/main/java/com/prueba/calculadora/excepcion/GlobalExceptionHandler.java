d2e18c1381c91bb4c5515f21d45025238e5d45ef
Lucas Fernandez <lucasferdani@gmail.com>
2022-09-15 22:48:49 -0300

    Implementacion de error personalizado para cuando se trata de dividir por 0

--- src/main/java/com/prueba/calculadora/excepcion/GlobalExceptionHandler.java
+++ src/main/java/com/prueba/calculadora/excepcion/GlobalExceptionHandler.java
@@ -19,47 +19,52 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-	
-	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-	
-	@ExceptionHandler(NumberFormatException.class)
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(NumberFormatException.class)
     public ResponseEntity<Object> handleFormatException(HttpServletRequest req, Exception ex) {
-		logger.error("Request: " + req.getRequestURL() + " ,Error: " + ex);
+        logger.error("Request: " + req.getRequestURL() + " ,Error: " + ex);
         return new ResponseEntity<>("Error de formato", HttpStatus.BAD_REQUEST);
-	}
-	
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity <Object> handleCustomAPIException(Exception ex, HttpHeaders headers, HttpStatus status, WebRequest req) {
-    
-		logger.error("Error: " + ex);
+    public ResponseEntity <Object> handleCustomAPIException(Exception ex) {
+        logger.error("Error: " + ex);
         return new ResponseEntity<>("La calculadora se ha freido", HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
+
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity <Object> resourceNotFoundException(ResourceNotFoundException ex) {
-    	logger.error(ex.getMessage());
+        logger.error(ex.getMessage());
         return new ResponseEntity<>("Not found", HttpStatus.NOT_FOUND);
     }
-    
+
+    @ExceptionHandler(ArithmeticException.class)
+    public ResponseEntity <Object> handleDivideByZero(ArithmeticException ex) {
+        logger.error("Error: " + ex);
+        return new ResponseEntity<>("No se puede dividir por 0", HttpStatus.BAD_REQUEST);
+    }
+
     @Override
     protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         logger.error(ex.getMessage());
         return new ResponseEntity<>("Bind error", HttpStatus.BAD_REQUEST);
     }
 
-	@Override
-	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		logger.error(ex.getMessage());
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+                                                        HttpStatus status, WebRequest request) {
+        logger.error(ex.getMessage());
         return new ResponseEntity<>("Type mismatch", HttpStatus.BAD_REQUEST);
-	}
+    }
 
-	@Override
-	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		logger.error(ex.getMessage());
-		return new ResponseEntity<>("La pagina no existe", HttpStatus.NOT_FOUND);
-	}
-   
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
+                                                                   HttpStatus status, WebRequest request) {
+        logger.error(ex.getMessage());
+        return new ResponseEntity<>("La pagina no existe", HttpStatus.NOT_FOUND);
+    }
+
 }
