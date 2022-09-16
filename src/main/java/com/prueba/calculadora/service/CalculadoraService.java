4620695b4e7416eff600cbfbd0a6b4491669e8e8
Lucas Fernandez <lucasferdani@gmail.com>
2022-09-15 22:44:00 -0300

    Implementacion de Services de multiplicacion y division

--- src/main/java/com/prueba/calculadora/service/CalculadoraService.java
+++ src/main/java/com/prueba/calculadora/service/CalculadoraService.java
@@ -4,58 +4,88 @@ import java.math.BigDecimal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.corp.calculator.TracerImpl;
 
 @Service
 public class CalculadoraService {
-	
-	//TODO
-	//@Autowired
-	TracerImpl tracerImpl;
 
-	BigDecimal resultado;
-	
+	@Autowired
+	private TracerImpl tracerImpl;
+
+	private BigDecimal resultado;
+
 	private static final Logger logger = LoggerFactory.getLogger(CalculadoraService.class);
-	
+
 	/**
 	 * Metodo que devuelve la suma de dos parametros
 	 * @param num1 parametro numero 1
 	 * @param num2 parametro numero 2
-	 * @return
+	 * @return resultado
 	 */
 	public double suma (double num1, double num2) {
-			
 		resultado = BigDecimal.valueOf(num1);
 		resultado = resultado.add(BigDecimal.valueOf(num2));
-		
-		tracerImpl = new TracerImpl();
-		tracerImpl.trace(resultado);
-		
-		logger.info("Traza: " + resultado);
-		
+
+		traceResult();
+
 		return resultado.doubleValue();
-					
+
 	}
-	
+
 	/**
 	 * Metodo que devuelve la resta de dos parametros
 	 * @param num1 parametro numero 1
 	 * @param num2 parametro numero 2
-	 * @return
+	 * @return resultado
 	 */
 	public double resta (double num1, double num2) {
-			
+
 		resultado = BigDecimal.valueOf(num1);
 		resultado = resultado.subtract(BigDecimal.valueOf(num2));
-	
-		tracerImpl = new TracerImpl();
-		tracerImpl.trace(resultado);
-		
-		logger.info("Traza: " + resultado);
-		
+
+		traceResult();
+
 		return resultado.doubleValue();
-			
+
+	}
+
+	/**
+	 * Metodo que devuelve la multiplicacion de dos parametros
+	 * @param num1 parametro numero 1
+	 * @param num2 parametro numero 2
+	 * @return resultado
+	 */
+	public double multiplicacionSimple(double num1, double num2) {
+		resultado = BigDecimal.valueOf(num1);
+		resultado = resultado.multiply(BigDecimal.valueOf(num2));
+
+		traceResult();
+
+		return resultado.doubleValue();
+	}
+
+	/**
+	 * Metodo que devuelve la division de dos parametros
+	 * @param num1 parametro numero 1
+	 * @param num2 parametro numero 2
+	 * @return resultado
+	 */
+	public double divisionSimple(double num1, double num2) {
+		resultado = BigDecimal.valueOf(num1);
+		resultado = resultado.divide(BigDecimal.valueOf(num2));
+
+		traceResult();
+
+		return resultado.doubleValue();
+	}
+
+	private void traceResult() {
+		tracerImpl = new TracerImpl();
+		tracerImpl.trace(this.resultado);
+
+		logger.info("Traza: " + this.resultado);
 	}
 }
